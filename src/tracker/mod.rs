2b0fc521cb27f37a595d3ab89b92aba6be6ffc28
José Manuel Díez <jose.manuel.diez@gmail.com>
2026-04-15 21:22:50 +0200

    tracker: rename speed -> range_rate

--- src/tracker/mod.rs
+++ src/tracker/mod.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use chrono::Utc;
 use clap::Args;
-use lox_space::units::SPEED_OF_LIGHT;
+use lox_space::{frames::providers::DefaultRotationProvider, units::SPEED_OF_LIGHT};
 use tokio::{sync::broadcast, time::sleep};
 use tracing::info;
 
@@ -15,6 +15,7 @@ use crate::{
     },
 };
 
+mod rotctl;
 mod update;
 mod utils;
 
@@ -32,11 +33,17 @@ pub struct TrackerArgs {
 
 /// Runs the tracker loop until stopped.
 pub async fn run(args: TrackerArgs, pdb: &PredictDb, config: &Config) {
-    // For each output
-    //  Create output task
-    //
     let (exit_tx, mut exit_rx) = broadcast::channel(1);
-    let (update_tx, update_rx) = broadcast::channel(1);
+    let (update_tx, _) = broadcast::channel(1);
+
+    for out in args.out.into_iter() {
+        match out {
+            Output::Rotctl(addr) => {
+                tokio::spawn(rotctl::run(addr, update_tx.subscribe()));
+            }
+            _ => todo!(),
+        }
+    }
 
     // Get the spacecraft we are tracking
     // and the GS
@@ -63,14 +70,17 @@ pub async fn run(args: TrackerArgs, pdb: &PredictDb, config: &Config) {
         // Compute observables at the current time for the GS
         let now = Utc::now();
         let state = pdb.state_at(now, sc).unwrap();
-        let observables = gs.location().observables_dyn(state);
+        let state_body_frame = state
+            .try_to_frame(gs.body_fixed_frame(), &DefaultRotationProvider)
+            .unwrap();
+        let observables = gs.location().observables_dyn(state_body_frame);
 
         // Compute Doppler corrected frequencies if present
-        let speed = observables.range_rate();
-        let tx_frequency_hertz = doppler_correct(args.tx_freq, speed, true);
-        let rx_frequency_hertz = doppler_correct(args.rx_freq, speed, false);
+        let range_rate = observables.range_rate();
+        let tx_frequency_hertz = doppler_correct(args.tx_freq, range_rate, true);
+        let rx_frequency_hertz = doppler_correct(args.rx_freq, range_rate, false);
 
-        // Create tracker update structure
+        // Create and send tracker update
         let update = Update {
             timestamp: now,
             azimuth_degrees: observables.azimuth().to_degrees(),
@@ -80,11 +90,15 @@ pub async fn run(args: TrackerArgs, pdb: &PredictDb, config: &Config) {
             tx_frequency_hertz,
             rx_frequency_hertz,
         };
-        info!(?name, "az={:.2} el={:.2}", update.azimuth_degrees, update.elevation_degrees);
-
-        update_tx
-            .send(update)
-            .expect("error sending tracker update");
+        info!(
+            ?name,
+            ?range_rate,
+            range = ?observables.range(),
+            "az={:.2} el={:.2}",
+            update.azimuth_degrees,
+            update.elevation_degrees
+        );
+        let _ = update_tx.send(update);
     }
 
     let _ = exit_tx.send(());
@@ -92,12 +106,13 @@ pub async fn run(args: TrackerArgs, pdb: &PredictDb, config: &Config) {
 
 fn doppler_correct(
     base_freq: Option<Frequency>,
-    speed_meters_per_second: f64,
+    range_rate_meters_per_second: f64,
     is_uplink: bool,
 ) -> Option<u64> {
     base_freq.map(|Frequency(freq)| {
         let carrier_lambda = SPEED_OF_LIGHT / (freq as f64);
-        let f_shift = if is_uplink { 1.0 } else { -1.0 } * speed_meters_per_second / carrier_lambda;
+        let f_shift =
+            if is_uplink { 1.0 } else { -1.0 } * range_rate_meters_per_second / carrier_lambda;
         freq.saturating_add_signed(f_shift as i64)
     })
 }
